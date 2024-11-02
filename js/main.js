001ceadb5339af72200d947f813891ee34b63deb
MarcoProstamo <Marcopros1201@gmail.com>
2024-11-02 17:07:46 +0100

    Feat → Dynamic Img Generation

--- js/main.js
+++ js/main.js
@@ -1,23 +1,60 @@
 // # Selecting DOM Elements
 const container = document.querySelector("#container");
 const jumboImg = document.querySelector("#jumboImg");
-const previewImgs = document.querySelectorAll("#previewImg");
+const previewImgContainer = document.querySelector("#previewImgContainer");
 const prevImg = document.querySelector("#prevImg");
 const nextImg = document.querySelector("#nextImg");
 
 // # Variable Init
+const imgs = [
+  {
+    alt: "img-01",
+    path: "./img/img-01.jpg",
+  },
+  {
+    alt: "img-02",
+    path: "./img/img-02.jpg",
+  },
+  {
+    alt: "img-03",
+    path: "./img/img-03.jpg",
+  },
+];
+
 let currentImgIndex = 0;
 
+// # Img Generation
+jumboImg.innerHTML = `<img src="./img/img-01.jpg" alt="img-01" />`;
+let previewImgHtml = `
+    <div class="preview-img active" id="previewImg">
+      <img src=${imgs[currentImgIndex].path} alt=${imgs[currentImgIndex].path} />
+    </div>
+  `;
+
+for (let i = 1; i < imgs.length; i++) {
+  const img = imgs[i];
+  previewImgHtml += `
+      <div class="preview-img" id="previewImg">
+        <img src=${img.path} alt=${img.alt} />
+      </div>
+    `;
+}
+
+previewImgContainer.innerHTML = previewImgHtml;
+
 // # Event Listener
+const previewImgs = document.querySelectorAll("#previewImg");
 
 // * Go Previous Image Via Arrow
 prevImg.addEventListener("click", () => {
   previewImgs[currentImgIndex].classList.remove("active");
+
   !currentImgIndex
     ? (currentImgIndex = previewImgs.length - 1)
     : currentImgIndex--;
+
   previewImgs[currentImgIndex].classList.add("active");
-  jumboImg.src = `./img/img-0${currentImgIndex + 1}.jpg`;
+  jumboImg.innerHTML = `<img src=${imgs[currentImgIndex].path} alt=${imgs[currentImgIndex].alt} />`;
 });
 
 // * Go Next Image Via Arrow
@@ -29,7 +66,7 @@ nextImg.addEventListener("click", () => {
     : currentImgIndex++;
 
   previewImgs[currentImgIndex].classList.add("active");
-  jumboImg.src = `./img/img-0${currentImgIndex + 1}.jpg`;
+  jumboImg.innerHTML = `<img src=${imgs[currentImgIndex].path} alt=${imgs[currentImgIndex].alt} />`;
 });
 
 // * Change Image by Clicking the Preview
@@ -38,7 +75,7 @@ previewImgs.forEach((previewImg, indexImg) => {
     previewImgs[currentImgIndex].classList.remove("active");
     currentImgIndex = indexImg;
     previewImgs[currentImgIndex].classList.add("active");
-    jumboImg.src = `./img/img-0${currentImgIndex + 1}.jpg`;
+    jumboImg.innerHTML = `<img src=${imgs[currentImgIndex].path} alt=${imgs[currentImgIndex].alt} />`;
   });
 });
 
@@ -50,8 +87,8 @@ autoPlay = setInterval(() => {
     ? (currentImgIndex = 0)
     : currentImgIndex++;
   previewImgs[currentImgIndex].classList.add("active");
-  jumboImg.src = `./img/img-0${currentImgIndex + 1}.jpg`;
-}, 2000);
+  jumboImg.innerHTML = `<img src=${imgs[currentImgIndex].path} alt=${imgs[currentImgIndex].alt} />`;
+}, 1500);
 
 container.addEventListener("mouseover", () => {
   clearInterval(autoPlay);
@@ -64,6 +101,6 @@ container.addEventListener("mouseleave", () => {
       ? (currentImgIndex = 0)
       : currentImgIndex++;
     previewImgs[currentImgIndex].classList.add("active");
-    jumboImg.src = `./img/img-0${currentImgIndex + 1}.jpg`;
-  }, 2000);
+    jumboImg.innerHTML = `<img src=${imgs[currentImgIndex].path} alt=${imgs[currentImgIndex].alt} />`;
+  }, 1500);
 });
